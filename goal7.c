b24fb80cfc228e70caaadd065b940332d03771da
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-06-13 22:36:50 +0530

    linked list traversal and

---
+++ goal7.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void linkedlisttraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    // allocate memry for nodes in  the linked list in the heap
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    // enter the value of first node and link it to the third node
+    head->data = 7;
+    head->next = second;
+
+    // linked the second node to the third node
+    second->data = 77;
+    second->next = third;
+
+    // third node is linked to fourth
+    third->data = 777;
+    third->next = fourth;
+
+    // fourth node is linked to null
+    fourth->data = 7777;
+    fourth->next = NULL;
+
+    linkedlisttraversal(head);
+    return 0;
+}
