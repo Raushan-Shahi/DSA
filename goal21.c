5abb3526ede2389bdb68125ee6b4f5b65ee5f503
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-07-16 20:48:01 +0530

    stack and doubly linked list

--- goal21.c
+++ goal21.c
@@ -1,62 +1,53 @@
+//stack implemantation with array 
+
+
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 struct stack
 {
     int size;
     int top;
     int *arr;
 };
-
-int isempty(struct stack *ptr)
+ 
+int isEmpty(struct stack ptr)
 {
-    if (ptr->top == -1)
+    if (ptr.top == -1)
     {
         return 1;
     }
-    else 
+    else
     {
         return 0;
     }
-};
-
-// int isfull(struct stack *ptr)
-// {
-//     if (ptr->top == ptr->size - 1)
-//     {
-//         return 1;
-//     }
-//     return 0;
-// };
-
-int main()
+}
+ 
+int isFull(struct stack *ptr)
 {
-    // struct stack s;
-    // s.size = 80;
-    // s.top = -1;
-    // s.arr = (int *)malloc(s.size * sizeof(int));
-
-    struct stack * s;
-    s->size = 80;
-    s->top = -1;
-    s->arr = (int *)malloc(s->size * sizeof(int));
-
-    printf("%d\n",s->size);
-
-    int c = isempty(s);
-
-    printf("%d\n",s->size);
-
-    if (c == 1 )
+    if (ptr->top == ptr->size - 1)
     {
-        printf("the stack is empty.");
+        return 1;
     }
     else
     {
-        printf("the stack is not empty.");
+        return 0;
     }
-
-    printf("%d\n",s->size);
-
+}
+ 
+int main()
+{
+    struct stack s;
+    s.size = 80;
+    s.top = -1;
+    s.arr = (int *)malloc(s.size * sizeof(int));
+    
+   // Check if stack is empty
+    if(isEmpty(s)){
+        printf("The stack is empty");
+    }
+    else{
+        printf("The stack is not empty");
+    }
     return 0;
 }
