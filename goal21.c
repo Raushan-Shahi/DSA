f77b737d48de00365a34aeb4d818c58bd411ea9e
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-07-16 23:39:30 +0530

    stack

--- goal21.c
+++ goal21.c
@@ -1,19 +1,18 @@
-//stack implemantation with array 
-
+// stack implemantation with array
 
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 struct stack
 {
     int size;
     int top;
     int *arr;
 };
- 
-int isEmpty(struct stack ptr)
+
+int isEmpty(struct stack *ptr)
 {
-    if (ptr.top == -1)
+    if (ptr->top == -1)
     {
         return 1;
     }
@@ -22,7 +21,7 @@ int isEmpty(struct stack ptr)
         return 0;
     }
 }
- 
+
 int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
@@ -34,19 +33,21 @@ int isFull(struct stack *ptr)
         return 0;
     }
 }
- 
+
 int main()
 {
-    struct stack s;
-    s.size = 80;
-    s.top = -1;
-    s.arr = (int *)malloc(s.size * sizeof(int));
-    
-   // Check if stack is empty
-    if(isEmpty(s)){
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+
+    // Check if stack is empty
+    if (isEmpty(s))
+    {
         printf("The stack is empty");
     }
-    else{
+    else
+    {
         printf("The stack is not empty");
     }
     return 0;
---
+++ goal22.c
@@ -0,0 +1,91 @@
+// stack implemantation with array
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isempty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+        return 0;
+};
+
+int isfull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+        return 0;
+};
+
+void push(struct stack * ptr, int val)
+{
+    if(isfull(ptr))
+    {
+        printf("the stack is overflowing, cannot push %d.\n",val);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+};
+
+int pop(struct stack * ptr)
+{
+    int val;
+    if(isempty(ptr))
+    {
+        printf("the stack is underflowing, cannot pop.\n");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->arr[ptr->top] = val;
+        ptr->top--;
+        return val;
+    }
+};
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("the stack has been created successfully.\n");
+    printf("%d, here 1 means full and 0 means not full\n", isfull(sp));
+    printf("%d, here 1 means empty and 0 means not empty\n", isempty(sp));
+    push(sp, 23);
+    push(sp, 24);
+    push(sp, 25);
+    push(sp, 26);
+    push(sp, 27);
+    push(sp, 28);
+    push(sp, 29);
+    push(sp, 30);
+    push(sp, 31);
+    push(sp, 32);
+    push(sp, 33);
+    push(sp, 34);
+    push(sp, 35);
+    printf("%d, here 1 means full and 0 means not full\n", isfull(sp));
+    printf("%d, here 1 means empty and 0 means not empty\n",isempty(sp));
+    printf("%d\n",pop(sp));
+    printf("%d\n",pop(sp));
+    printf("%d\n",pop(sp));
+    printf("%d, here 1 means full and 0 means not full\n", isfull(sp));
+    printf("%d, here 1 means empty and 0 means not empty\n",isempty(sp));
+    return 0;
+}
